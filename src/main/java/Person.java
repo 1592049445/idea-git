af4bc74bae646a633ab07d34ca5d4040cae08beb
njn <1592049445@qq.com>
2018-07-11 16:42:23 +0800

    "分支提交"

---
+++ src/main/java/Person.java
@@ -0,0 +1,14 @@
+/**
+ * Created by njn on 2018/7/11.
+ */
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
