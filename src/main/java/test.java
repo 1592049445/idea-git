ba9b8737400bd762ccd765deff37d2569c8f6423
njn <1592049445@qq.com>
2018-07-11 16:21:00 +0800

    "第二次提交"

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,4 +2,17 @@
  * Created by njn on 2018/7/11.
  */
 public class test {
+    private String id;
+
+    public test(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
